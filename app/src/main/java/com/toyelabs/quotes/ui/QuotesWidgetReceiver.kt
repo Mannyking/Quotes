41663d8d7a4ac22885335053de5bfcffccfd67f8
Mannyking <68853328+Mannyking@users.noreply.github.com>
2025-09-23 21:47:14 -0400

    Initial Glance AppWidget setup
    - AndroidManifest.xml
    -- added receiver block defining our widget specs
    
    - build.gradle.kts
    - libs.versions.toml
    -- new dependency, glance, for widget creation and handling
    
    - quotes_widget_info.xml
    -- config to be used by phone widget picker
    
    - QuotesWidgetReceiver.kt
    -- acts as a bridge between device and actual widget impl(not yet done)

---
+++ app/src/main/java/com/toyelabs/quotes/ui/QuotesWidgetReceiver.kt
@@ -0,0 +1,8 @@
+package com.toyelabs.quotes.ui
+
+import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class QuotesWidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget: GlanceAppWidget = TODO("Not yet implemented")
+}
