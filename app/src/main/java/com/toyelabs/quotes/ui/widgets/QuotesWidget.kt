3beb3ed029685f19a1f1d7965b66b1154dedf9dd
Mannyking <68853328+Mannyking@users.noreply.github.com>
2025-10-29 23:45:48 -0400

    Refactor widget implementation and cleanup manifest
    - AndroidManifest.xml
    -- removed redundant `MainActivity` declaration and associated intent filter
    
    - QuotesWidget.kt
    -- added `GlanceTheme` support for consistent theming
    -- incorporated `GlanceTheme.colors.background` for widget background
    
    - quotes_widget_info.xml
    -- removed unused `android:initialLayout` attribute

--- app/src/main/java/com/toyelabs/quotes/ui/widgets/QuotesWidget.kt
+++ app/src/main/java/com/toyelabs/quotes/ui/widgets/QuotesWidget.kt
@@ -4,10 +4,12 @@ import android.content.Context
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
+import androidx.glance.GlanceTheme
 import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
@@ -18,6 +20,7 @@ import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
 import androidx.glance.layout.padding
 import androidx.glance.text.Text
+import androidx.glance.unit.ColorProvider
 import com.toyelabs.quotes.MainActivity
 
 class QuotesWidget : GlanceAppWidget() {
@@ -29,20 +32,23 @@ class QuotesWidget : GlanceAppWidget() {
 
     @Composable
     private fun MyContent() {
-        Column(
-            modifier = GlanceModifier
-                .fillMaxSize()
-                .padding(8.dp),
-            verticalAlignment = Alignment.Top,
-            horizontalAlignment = Alignment.CenterHorizontally,
-        ) {
-            Text(
-                text = "Hello World",
-                modifier = GlanceModifier.fillMaxSize().padding(12.dp)
-            )
-            Row {
-                Button(text = "Button 1", onClick = actionStartActivity<MainActivity>())
-                Button(text = "Button 2", onClick = actionStartActivity<MainActivity>())
+        GlanceTheme {
+            Column(
+                modifier = GlanceModifier
+                    .fillMaxSize()
+                    .background(GlanceTheme.colors.background)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.Top,
+                horizontalAlignment = Alignment.CenterHorizontally,
+            ) {
+                Text(
+                    text = "Hello World",
+                    modifier = GlanceModifier.padding(12.dp)
+                )
+                Row {
+                    Button(text = "Button 1", onClick = actionStartActivity<MainActivity>())
+                    Button(text = "Button 2", onClick = actionStartActivity<MainActivity>())
+                }
             }
         }
     }
