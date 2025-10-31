1cfd892972376d13a9e9c5c741eb80fd6a054149
Mannyking <68853328+Mannyking@users.noreply.github.com>
2025-10-30 22:38:19 -0400

    Display quotes dynamically in widget
    - QuotesWidget.kt
    -- updated to fetch and display quotes dynamically from `LocalQuotesDataSource`
    -- improved layout with quote text and author styling
    
    - quotes.json
    -- expanded with additional quotes for variety

--- app/src/main/java/com/toyelabs/quotes/ui/widgets/QuotesWidget.kt
+++ app/src/main/java/com/toyelabs/quotes/ui/widgets/QuotesWidget.kt
@@ -1,37 +1,36 @@
 package com.toyelabs.quotes.ui.widgets
 
 import android.content.Context
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
-import androidx.glance.Button
 import androidx.glance.GlanceId
 import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
-import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.provideContent
 import androidx.glance.background
 import androidx.glance.layout.Alignment
 import androidx.glance.layout.Column
-import androidx.glance.layout.Row
 import androidx.glance.layout.fillMaxSize
+import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
+import androidx.glance.text.FontStyle
 import androidx.glance.text.Text
-import androidx.glance.unit.ColorProvider
-import com.toyelabs.quotes.MainActivity
+import androidx.glance.text.TextAlign
+import androidx.glance.text.TextStyle
+import com.toyelabs.quotes.data.LocalQuotesDataSource
+import com.toyelabs.quotes.data.Quote
 
 class QuotesWidget : GlanceAppWidget() {
     override suspend fun provideGlance(context: Context, id: GlanceId) {
+        val quote = LocalQuotesDataSource(context).getQuotes();
         provideContent {
-            MyContent()
+            MyContent(quote)
         }
     }
 
     @Composable
-    private fun MyContent() {
+    private fun MyContent(quote: Quote) {
         GlanceTheme {
             Column(
                 modifier = GlanceModifier
@@ -39,16 +38,25 @@ class QuotesWidget : GlanceAppWidget() {
                     .background(GlanceTheme.colors.background)
                     .padding(8.dp),
                 verticalAlignment = Alignment.Top,
-                horizontalAlignment = Alignment.CenterHorizontally,
             ) {
                 Text(
-                    text = "Hello World",
-                    modifier = GlanceModifier.padding(12.dp)
+                    text = quote.text,
+                    modifier = GlanceModifier.padding(12.dp),
+                    style = TextStyle(
+                        color = GlanceTheme.colors.onBackground
+                    )
                 )
-                Row {
-                    Button(text = "Button 1", onClick = actionStartActivity<MainActivity>())
-                    Button(text = "Button 2", onClick = actionStartActivity<MainActivity>())
-                }
+                Text(
+                    text = quote.author,
+                    modifier = GlanceModifier
+                        .fillMaxWidth()
+                        .padding(12.dp),
+                    style = TextStyle(
+                        fontStyle = FontStyle.Italic,
+                        color = GlanceTheme.colors.onBackground,
+                        textAlign = TextAlign.End
+                    )
+                )
             }
         }
     }
